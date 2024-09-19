46a536d39b867ac561494e347322cc3abbbf930f
Daishan Peng <daishan@acorn.io>
2024-09-19 08:56:58 -0700

    Use colly to scrape website
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
+	url2 "net/url"
 	"os"
 	"path"
-	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/mendableai/firecrawl-go"
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/gocolly/colly"
 	"github.com/sirupsen/logrus"
 )
 
@@ -23,7 +20,7 @@ type Metadata struct {
 }
 
 type MetadataInput struct {
-	URL string `json:"url"`
+	URLs []string `json:"urls"`
 }
 
 type MetadataOutput struct {
@@ -40,19 +37,7 @@ type PageDetails struct {
 }
 
 func main() {
-	firecrawlUrl := os.Getenv("FIRECRAWL_URL")
-	if firecrawlUrl == "" {
-		firecrawlUrl = "http://localhost:3002"
-	}
-	apiKey := os.Getenv("FIRECRAWL_API_KEY")
-	if apiKey == "" {
-		apiKey = "test"
-	}
-	app, err := firecrawl.NewFirecrawlApp(apiKey, firecrawlUrl)
-	if err != nil {
-		log.Fatalf("Failed to initialize FirecrawlApp: %v", err)
-	}
-
+	var err error
 	workingDir := os.Getenv("GPTSCRIPT_WORKSPACE_DIR")
 	if workingDir == "" {
 		workingDir, err = os.Getwd()
@@ -81,135 +66,79 @@ func main() {
 		metadata.Output.Pages = make(map[string]PageDetails)
 	}
 
-	if metadata.Output.ScrapeJobId == "" {
-		crawlStatus, err := app.AsyncCrawlURL(metadata.Input.URL, &firecrawl.CrawlParams{
-			Limit: &[]int{100}[0],
-			ScrapeOptions: firecrawl.ScrapeParams{
-				Formats: []string{"markdown"},
-			},
-		}, nil)
+	collector := colly.NewCollector()
+	converter := md.NewConverter("", true, nil)
+
+	visited := make(map[string]bool)
+
+	for _, url := range metadata.Input.URLs {
+		collector.OnHTML("body", func(e *colly.HTMLElement) {
+			if visited[e.Request.URL.String()] {
+				return
+			}
+			logrus.Infof("scraping %s", e.Request.URL.String())
+			visited[e.Request.URL.String()] = true
+			markdown := converter.Convert(e.DOM)
+			hostname := e.Request.URL.Hostname()
+			urlPath := e.Request.URL.Path
+
+			var filePath string
+			if urlPath == "/" {
+				filePath = path.Join(workingDir, hostname, "index.md")
+			} else {
+				trimmedPath := strings.Trim(urlPath, "/")
+				segments := strings.Split(trimmedPath, "/")
+				fileName := segments[len(segments)-1] + ".md"
+				filePath = path.Join(path.Join(workingDir, hostname, strings.Join(segments[:len(segments)-1], "/")), fileName)
+			}
+			dirPath := path.Dir(filePath)
+			err := os.MkdirAll(dirPath, os.ModePerm)
+			if err != nil {
+				logrus.Errorf("Failed to create directories for %s: %v", dirPath, err)
+				return
+			}
+
+			err = os.WriteFile(filePath, []byte(markdown), 0644)
+			if err != nil {
+				logrus.Errorf("Failed to write markdown to %s: %v", filePath, err)
+				return
+			}
+			visited[e.Request.URL.String()] = true
+
+			metadata.Output.Pages[e.Request.URL.Path] = PageDetails{
+				Path:       filePath,
+				URL:        e.Request.URL.String(),
+				LastUpdate: time.Now().String(),
+			}
+		})
+
+		collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+			link := e.Attr("href")
+			baseURL, err := url2.Parse(url)
+			if err != nil {
+				logrus.Errorf("Invalid base URL: %v", err)
+				return
+			}
+			linkURL, err := url2.Parse(link)
+			if err != nil {
+				logrus.Errorf("Invalid link URL %s: %v", link, err)
+				return
+			}
+			if linkURL.Host == "" || baseURL.Host == linkURL.Host {
+				e.Request.Visit(link)
+			}
+		})
+
+		err := collector.Visit(url)
 		if err != nil {
-			log.Fatalf("Failed to send crawl request: %v", err)
+			logrus.Errorf("Failed to scrape URL %s: %v", url, err)
 		}
-		metadata.Output.ScrapeJobId = crawlStatus.ID
+
 		if err := writeMetadata(metadata, metadataPath); err != nil {
 			log.Fatalf("Failed to write metadata: %v", err)
 		}
 	}
 
-	fileWritten := 0
-OuterLoop:
-	for {
-		crawlStatus, err := app.CheckCrawlStatus(metadata.Output.ScrapeJobId)
-		if err != nil {
-			log.Fatalf("Failed to check crawl status: %v", err)
-		}
-		if crawlStatus.Status == "completed" {
-			for {
-				for _, data := range crawlStatus.Data {
-					source := ""
-					if data.Metadata.SourceURL != nil {
-						source = *data.Metadata.SourceURL
-					}
-					lastModified := time.Now().String()
-					if data.Metadata.ModifiedTime != nil {
-						lastModified = *data.Metadata.ModifiedTime
-					}
-
-					parsedURL, err := url.Parse(source)
-					if err != nil {
-						logrus.Errorf("Failed to parse URL %s: %v", source, err)
-						continue
-					}
-
-					urlPath := ""
-					if parsedURL.Path == "" {
-						urlPath = "/" + parsedURL.Host
-					} else {
-						urlPath = parsedURL.Path
-					}
-
-					existingPage, ok := metadata.Output.Pages[urlPath]
-					if ok && existingPage.LastUpdate == lastModified && lastModified != "" {
-						continue
-					}
-
-					markdownPath := filepath.Join(workingDir, parsedURL.Host, urlPath) + ".md"
-
-					if err := os.MkdirAll(filepath.Dir(markdownPath), 0755); err != nil {
-						logrus.Errorf("Failed to create directory for %s: %v", markdownPath, err)
-						continue
-					}
-
-					if err := os.WriteFile(markdownPath, []byte(data.Markdown), 0644); err != nil {
-						logrus.Errorf("Failed to write markdown file %s: %v", markdownPath, err)
-						continue
-					}
-
-					metadata.Output.Pages[urlPath] = PageDetails{
-						LastUpdate: lastModified,
-						Path:       markdownPath,
-						URL:        source,
-					}
-					fileWritten++
-					logrus.Infof("wrote %d webpages to disk", fileWritten)
-					metadata.Output.Status = fmt.Sprintf("wrote %d webpages to disk", fileWritten)
-
-					if err := writeMetadata(metadata, metadataPath); err != nil {
-						logrus.Fatalf("Failed to write metadata: %v", err)
-					}
-				}
-				if crawlStatus.Next == nil || *crawlStatus.Next == "" {
-					break OuterLoop
-				} else {
-					if !strings.HasPrefix(*crawlStatus.Next, "http://") {
-						*crawlStatus.Next = "http://" + strings.TrimPrefix(*crawlStatus.Next, "https://")
-					}
-
-					req, err := http.NewRequest(http.MethodGet, *crawlStatus.Next, nil)
-					if err != nil {
-						log.Fatalf("Failed to create request: %v", err)
-					}
-					req.Header.Add("Content-Type", "application/json")
-					client := &http.Client{}
-					var respBody []byte
-					for i := 0; i < 3; i++ {
-						resp, err := client.Do(req)
-						if err != nil {
-							time.Sleep(500 * time.Millisecond)
-							continue
-						}
-						defer resp.Body.Close()
-						respBody, err = io.ReadAll(resp.Body)
-						if err != nil {
-							log.Fatalf("Failed to read response body: %v", err)
-						}
-						break
-					}
-					if err != nil {
-						log.Fatalf("Failed to get next crawl status: %v", err)
-					}
-					newCrawlStatus := firecrawl.CrawlStatusResponse{}
-					err = json.Unmarshal(respBody, &newCrawlStatus)
-					if err != nil {
-						log.Fatalf("Failed to unmarshal next crawl status: %v", err)
-					}
-					crawlStatus = &newCrawlStatus
-				}
-			}
-		} else {
-			metadata.Output.Status = fmt.Sprintf("crawling status: %s, completed %d, total %d", crawlStatus.Status, crawlStatus.Completed, crawlStatus.Total)
-			if err := writeMetadata(metadata, metadataPath); err != nil {
-				log.Fatalf("Failed to write metadata: %v", err)
-			}
-			continue
-		}
-	}
-
-	metadata.Output.Status = "done"
-	if err := writeMetadata(metadata, metadataPath); err != nil {
-		log.Fatalf("Failed to write metadata: %v", err)
-	}
 }
 
 func writeMetadata(metadata Metadata, path string) error {
